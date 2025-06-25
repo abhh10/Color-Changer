99413e909ccb6716e6980ba253ff0cef47cb4c74
abhh10 <130175061+abhh10@users.noreply.github.com>
2025-06-25 21:59:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const colors = document.querySelectorAll('.square'); 
+const bod = document.querySelector('body');
+
+colors.forEach(function(color) {
+    color.addEventListener('click', function(e) {
+        if (e.target.className === "square red") {
+            bod.style.backgroundColor = "red";
+        }
+        if (e.target.className === "square blue") {
+            bod.style.backgroundColor = "blue";
+        }
+        if (e.target.className === "square green") {
+            bod.style.backgroundColor = "green";
+        }
+        if (e.target.className === "square yellow") {
+            bod.style.backgroundColor = "yellow";
+        }
+    });
+});
 
